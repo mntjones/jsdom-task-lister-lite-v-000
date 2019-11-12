950bf6d638fd0ad9fe25ede127d13229a0651da3
Monica Tamaru Jones <mntamaru@gmail.com>
2019-11-12 22:06:52 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -61,3 +61,17 @@ const appendNewTask = task => {
 //   });
 // });
 
+// class Task {
+//   constructor(description) {
+//     this.description = description;
+//   }
+
+//   render() {
+//     return `
+//       <li>
+//         ${this.description}
+//         <button data-description="${this.description}">X</button>
+//       </li>
+//       `;
+//   }
+// }
